b04fcae59d73c99f17ffd5087abba25891255d18
masataka.koyama <mk.expfac.uj@gmail.com>
2020-05-15 08:34:23 +0100

    a

---
+++ public/test.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+use Twilio\Rest\Client;
+
+// Your Account SID and Auth Token from twilio.com/console
+$account_sid = 'ACf50e70af7ff62a59e8564078da0cdbdb';
+$auth_token = '7e0a55dc363febce431303fe78ef9c9e';
+// In production, these should be environment variables. E.g.:
+// $auth_token = $_ENV["TWILIO_ACCOUNT_SID"]
+
+// A Twilio number you own with Voice capabilities
+$twilio_number = "+12066874931";
+
+// Where to make a voice call (your cell phone?)
+$to_number = "+819060108898";
+
+$client = new Client($account_sid, $auth_token);
+$client->account->calls->create(  
+    $to_number,
+    $twilio_number,
+    array(
+        "url" => "http://demo.twilio.com/docs/voice.xml"
+    )
+);
